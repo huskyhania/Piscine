f6d033cabd6c644ad77c1eae33f7b4cce2185971
huskyhania <h.skrzypiec@gmail.com>
2024-05-18 14:34:40 +0300

    Add files via upload

---
+++ rush00submittedversion/ex00/rush01.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhirvon <jhirvone@student.hive.fi>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/17 10:30:10 by jhirvon           #+#    #+#             */
+/*   Updated: 2024/02/18 17:53:26 by jhirvon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char a);
+
+void	print_row(int x, char left, char middle, char right)
+{
+	int	start;
+
+	start = 1;
+	while (start <= x)
+	{
+		if (start == 1)
+		{
+			ft_putchar(left);
+			start++;
+		}
+		if (start <= x -1)
+		{
+			ft_putchar(middle);
+			start++;
+		}
+		if (start == x)
+		{
+			ft_putchar(right);
+			start++;
+		}
+	}
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	int	start;
+
+	start = 1;
+	if (x < 1 || y < 1)
+	{
+		return ;
+	}
+	while (start <= y)
+	{
+		if (start == 1)
+		{
+			print_row(x, '/', '*', '\\');
+			start++;
+		}
+		if (start <= y -1)
+		{
+			print_row(x, '*', ' ', '*');
+			start++;
+		}
+		if (start == y)
+		{
+			print_row(x, '\\', '*', '/');
+			start++;
+		}
+	}
+}
